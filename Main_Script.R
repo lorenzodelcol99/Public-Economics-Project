71151c2a98c7ccb22ff802b6decb37e2e7f1a188
lorenzodelcol99 <lorenzodelcol99@gmail.com>
2025-12-10 11:54:12 +0100

    nuovo commento

--- Main_Script.R
+++ Main_Script.R
@@ -1,1 +1,7 @@
-print("Primo test di connessione con GitHub")
+# Uploading the Ferrier & Navvarro dataset
+data <- read.csv("Dataset/DATA_MACRO_FN.csv")
+
+head(data)
+
+# ok, scriviamo un commento a caso per 
+# vedere se riesco a vedere le verisoni dello script nel tempo
